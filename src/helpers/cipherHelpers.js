1131fe384dd36c78dce63cb745833c4cc08e7c56
ruairidhflint <roryflintphoto@gmail.com>
2021-04-18 20:27:08 +0100

    Fix vulnerabilities and remove unnecessary escape characters

--- src/helpers/cipherHelpers.js
+++ src/helpers/cipherHelpers.js
@@ -3,67 +3,60 @@ const alphabet = 'abcdefghijklmnopqrstuvwxyz-';
 function cypherKeyword(word) {
   const keyword = word
     .toLowerCase()
-    .replace(/[., \/#!$%\?!^&\*;:{}=\_-`~()]/g, "")
-    .split("")
-    .filter((item, index, array) => array.indexOf(item) == index)
-    .join("")
+    .replace(/[., /#!$%?!^&*;:{}=_-`~()]/g, '')
+    .split('')
+    .filter((item, index, array) => array.indexOf(item) === Number(index))
+    .join('');
 
-  let newString = alphabet
+  let newString = alphabet;
 
-  keyword.split('').forEach(char => {
-    newString = newString.replace(char, "")
-  })
+  keyword.split('').forEach((char) => {
+    newString = newString.replace(char, '');
+  });
 
-  return keyword + newString
+  return keyword + newString;
 }
 
 export function messageSubstitution(message, keyword) {
-
   const cypher = cypherKeyword(keyword);
 
   const message_input = message
     .toLowerCase()
-    .replace(/ /g, "-")
-    .replace(/[.,\/#!$%\?!^&\*;:{}=\_`~()]/g, "")
+    .replace(/ /g, '-')
+    .replace(/[.,/#!$%?!^&*;:{}=_`~()]/g, '')
     .split('')
-    .map(letter => {
-      return alphabet.indexOf(letter)
+    .map((letter) => {
+      return alphabet.indexOf(letter);
     });
 
   const codedMessage = message_input
-    .map(num => {
-      return cypher[num]
+    .map((num) => {
+      return cypher[num];
     })
-    .join("")
-    .replace(/-/g," ");
+    .join('')
+    .replace(/-/g, ' ');
 
-  return codedMessage
+  return codedMessage;
 }
 
 export function messageDecoder(codedMessage, keyword) {
-
   const cypher = cypherKeyword(keyword);
 
   const message_input = codedMessage
     .toLowerCase()
-    .replace(/ /g, "-")
-    .replace(/[.,\/#!$%\?!^&\*;:{}=\_`~()]/g, "")
+    .replace(/ /g, '-')
+    .replace(/[.,/#!$%?!^&*;:{}=_`~()]/g, '')
     .split('')
-    .map(letter => {
-      return cypher.indexOf(letter)
+    .map((letter) => {
+      return cypher.indexOf(letter);
     });
 
   const decodedMessage = message_input
-    .map(num => {
-      return alphabet[num]
+    .map((num) => {
+      return alphabet[num];
     })
-    .join("")
-    .replace(/-/g," ");
+    .join('')
+    .replace(/-/g, ' ');
 
-  return decodedMessage
+  return decodedMessage;
 }
-
-
-
-
-
